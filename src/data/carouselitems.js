67b5a5d201f90ffc5754f4cabab4b04161e985bc
Per-Olof Bengtsson <po@niffic.se>
2025-03-07 14:38:40 +0100

    improvements for 2025

---
+++ src/data/carouselitems.js
@@ -0,0 +1,19 @@
+const carouselItems = [
+  {
+    title: 'Welcome to the Fantasy Creatures Shop',
+    description: 'Dive into the enchanting world of Fantasy Creatures Shop, where every corner unveils a new magical being waiting to become your companion. Embark on an adventure with us and find your next mystical friend today!',
+    image: './assets/fantasy-creatures-shop.jpeg',
+  },
+  {
+    title: 'How to Care for Your Dragon',
+    description: 'Dragons are not just pets; they are loyal companions and formidable guardians. Learn the essentials of dragon care, from the fireproofing your home to the art of communication with these majestic creatures. Ensure your dragon thrives in the modern world with our comprehensive guide.',
+    image: './assets/me-and-my-dragon.jpeg',
+  },
+  {
+    title: 'Exclusive Offer: Mythical Creatures',
+    description: 'For a fleeting moment, the gates to the extraordinary creatures bazaar are open for you. Seize this chance to adopt a mythical creature at unprecedented prices. Discover beings of legend like the graceful unicorn, the mischievous pixie, or the wise griffin. Enhance your life with magic and mystery now!',
+    image: './assets/creatures-bazar-2.jpeg',
+  },
+]
+
+export default carouselItems
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,47 +1,34 @@
 <template>
-  <v-container fluid>
-    <v-row justify="center">
-      <v-col cols="12">
-        <v-carousel cycle height="80vh" hide-delimiters show-arrows="hover">
-          <v-carousel-item v-for="(item, i) in carouselItems" :key="i">
-            <v-img :src="item.image" aspect-ratio="1">
-              <template v-slot:placeholder>
-                <v-row class="fill-height ma-0" justify="center">
-                  <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                </v-row>
-              </template>
-              <v-row class="fill-height" justify="center">
-                <v-col class="text-h4 white-text text-center" cols="8">
-                  <div><strong>{{ item.title }}</strong></div>
-                  <div>{{ item.description }}</div>
-                </v-col>
-              </v-row>
-            </v-img>
-          </v-carousel-item>
-        </v-carousel>
-      </v-col>
-    </v-row>
+  <v-container>
+    <v-carousel cycle interval="5000" height="80vh" hide-delimiters show-arrows="hover">
+      <v-carousel-item v-for="(item, i) in carouselItems" :key="i">
+        <v-img :src="item.image">
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" justify="center">
+              <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+            </v-row>
+          </template>
+          <template v-slot:default>
+            <v-container>
+              <v-spacer></v-spacer>
+              <v-sheet rounded class="d-flex align-center justify-center flex-wrap text-center mx-auto px-4"
+                style="background: rgba(55, 33, 55, 0.4);">
+                <div style="color: gainsboro">
+                  <h3>{{ item.title }}</h3>
+                  <p>{{ item.description }}</p>
+                </div>
+              </v-sheet>
+            </v-container>
+          </template>
+        </v-img>
+      </v-carousel-item>
+    </v-carousel>
   </v-container>
 </template>
 
 <script setup>
-const carouselItems = [
-      {
-        title: 'Welcome to the Fantasy Creatures Shop',
-        description: 'Dive into the enchanting world of Fantasy Creatures Shop, where every corner unveils a new magical being waiting to become your companion. Embark on an adventure with us and find your next mystical friend today!',
-        image: './assets/fantasy-creatures-shop.jpeg',
-      },
-      {
-        title: 'How to Care for Your Dragon',
-        description: 'Dragons are not just pets; they are loyal companions and formidable guardians. Learn the essentials of dragon care, from the fireproofing your home to the art of communication with these majestic creatures. Ensure your dragon thrives in the modern world with our comprehensive guide.',
-        image: './assets/me-and-my-dragon.jpeg',
-      },
-      {
-        title: 'Exclusive Offer: Mythical Creatures',
-        description: 'For a fleeting moment, the gates to the extraordinary creatures bazaar are open for you. Seize this chance to adopt a mythical creature at unprecedented prices. Discover beings of legend like the graceful unicorn, the mischievous pixie, or the wise griffin. Enhance your life with magic and mystery now!',
-        image: './assets/creatures-bazar-2.jpeg',
-      },
-    ]
+import carouselItems from '@/data/carouselitems'
+
 </script>
 
 <style>
